0bb2a8434c219b7d7ef4a8e8882ff9ed330e435e
contygm <contygm@gmail.com>
2019-01-21 17:16:42 -0800

    lab2: setting up required files

---
+++ 2week/lab2/main.cpp
@@ -0,0 +1,13 @@
+/********************************************************************* 
+** Program name: TODO:
+** Author: Genevieve Conty
+** Date: 01/20/2019
+** Description: TODO: 
+*********************************************************************/
+#include <iostream>
+// #include ".hpp"
+
+int main() 
+{	  
+    return 0;
+}
---
+++ 2week/lab2/output-letters.hpp
@@ -0,0 +1,13 @@
+/********************************************************************* 
+** Program: TODO: 
+** Description: TODO: 
+*********************************************************************/ 
+
+#ifndef OUTPUTLETTERS_H
+#define OUTPUTLETTERS_H
+#include <fstream>
+using std::ofstream;
+
+void output_letters(ofstream&, int*);
+
+#endif
