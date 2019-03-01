679aca5bac12936c7b100930ea36640d9ff69bf2
contygm <contygm@gmail.com>
2019-02-28 19:32:44 -0800

    p4: validation loops work properly

--- projects/4-complete-fantasy-combat/validate.cpp
+++ projects/4-complete-fantasy-combat/validate.cpp
@@ -9,43 +9,51 @@
 
 using std::cout;
 using std::cin;
+using std::endl;
 
 // returns valid integer between min and max
 int getInteger()
 {
+    cout << "YO get int" << endl;
     double testInput;
     cin >> testInput;
 
     if (cin.fail() || testInput-floor(testInput)) 
     {
-        cout << "\033[1;31m ERROR: Please enter an integer value \033[0m\n";
+        cout << "\033[1;31m ERROR: Please enter an integer value \033[0m\n" << endl;
         cin.clear();
         cin.ignore(10000, '\n');
-        getInteger(); //loop back to begin input process
+        return getInteger(); //loop back to begin input process
     }
-
-    int input = (int)testInput;
-
-    return input;
+    else
+    {
+        int input = (int)testInput;
+        cout << "get int input: " << input << endl;
+        
+        return input;
+    }
 }
 
 // returns valid integer between min and max
 int getIntegerBetween(int min, int max)
 {
+    cout << "between input YO" << endl;
+
     // get valid integer
     int input = getInteger();
-
+    cout << "between input: " << input << endl;
+    
     // check that integer is between min and max
     if (input > max)
     {
-        cout << "\033[1;31m ERROR: Please enter an integer that is below or equal to "<< max <<" \033[0m\n";
-        getIntegerBetween(min, max); //loop back to begin input process
+        cout << "\033[1;31m ERROR: Please enter an integer that is below or equal to "<< max <<" \033[0m\n" << endl;
+        return getIntegerBetween(min, max); //loop back to begin input process
     }
 
     if (input < min)
     {
-        cout << "\033[1;31m ERROR: Please enter an integer that is above or equal to "<< min <<" \033[0m\n";
-        getIntegerBetween(min, max); //loop back to begin input process
+        cout << "\033[1;31m ERROR: Please enter an integer that is above or equal to "<< min <<" \033[0m\n" << endl;
+        return getIntegerBetween(min, max); //loop back to begin input process
     }
 
     return input;
