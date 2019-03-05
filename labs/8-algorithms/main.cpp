80b0d6e22bb61328d2fe6b84a321f7268a151452
Genevieve Conty <contygm@gmail.com>
2019-03-04 20:01:34 -0800

    L8: main file done

--- labs/8-algorithms/main.cpp
+++ labs/8-algorithms/main.cpp
@@ -7,12 +7,10 @@
 
 #include "validate.hpp"
 #include "searchAndSort.hpp"
-// #include <fstream>
-// #include <string>
+#include <iostream>
 
-// using std::ifstream;
-// using std::ofstream;
-// using std::string;
+using std::cout;
+using std::endl;
 
 int main()
 {
@@ -28,7 +26,47 @@ int main()
     int midArr[10];
     int endArr[10];
 
+    // simple search
+    cout << "\033[1;35mSIMPLE SEARCH: \033[0m" << endl;
+    cout << "\n\033[0;32mnormal array: \033[0m";
+    simpleSearch(normalArr);
+    
+    cout << "\n\033[0;32mearly array: \033[0m";
+    simpleSearch(earlyArr);
 
+    cout << "\n\033[0;32mmiddle array: \033[0m";
+    simpleSearch(midArr);
+
+    cout << "\n\033[0;32mend array: \033[0m";
+    simpleSearch(endArr);
+
+    // bubble sort
+    cout << "\n\033[1;35mBUBBLE SORT: \033[0m" << endl;
+    cout << "\n\033[0;32mnormal array \033[0m";
+    bubbleSort(normalArr);
+    
+    cout << "\n\033[0;32mearly array \033[0m";
+    bubbleSort(earlyArr);
+
+    cout << "\n\033[0;32mmiddle array \033[0m";
+    bubbleSort(midArr);
+
+    cout << "\n\033[0;32mend array \033[0m";
+    bubbleSort(endArr);
+
+    // binary sort
+    cout << "\n\033[1;35mBINARY SORT: \033[0m" << endl;
+    cout << "\n\033[0;32mnormal array: \033[0m";
+    binarySearchMenu(normalArr);
+    
+    cout << "\n\033[0;32mearly array: \033[0m";
+    binarySearchMenu(earlyArr);
+
+    cout << "\n\033[0;32mmiddle array: \033[0m";
+    binarySearchMenu(midArr);
+
+    cout << "\n\033[0;32mend array: \033[0m";
+    binarySearchMenu(endArr);
     
     return 0;
 }
