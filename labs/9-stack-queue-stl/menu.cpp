b22aadb399e76132ee9e79c4fd92f4d9c8877a7a
contygm <contygm@gmail.com>
2019-03-10 17:18:56 -0700

    L8: menus done for buffer and palindrome

--- labs/9-stack-queue-stl/menu.cpp
+++ labs/9-stack-queue-stl/menu.cpp
@@ -18,9 +18,9 @@ void mainMenu()
 {
     double testInput;
     // colorful menu with prompts for function choice
-    cout << "\033[35mWhich function would you like to use?\033[0m"
-    << "\n   \033[35m1\033[0m: Test the buffer"
-    << "\n   \033[35m2\033[0m: Create a palindrome"
+    cout << "\033[1;35mWhich function would you like to use?\033[0m"
+    << "\n   \033[0;35m1\033[0m: Test the buffer"
+    << "\n   \033[0;35m2\033[0m: Create a palindrome"
     << "\n   0: Exit the program" << endl;
 
     int input = getInteger();
@@ -35,23 +35,38 @@ void mainMenu()
             palindrome();
             break;
         default:
-            cout << "Have a nice day!" << endl;
+            cout << "\033[0;33mHave a nice day! :D\033[0m" << endl;
     }
 }
 
 void buffer()
 {
-    
-    cout << "\n\033[36mWhat string would you like to reverse?\033[0m" << endl;
-    int input; 
+    // menu section
+    cout << "\n\033[1;36mFor the simulation:\033[0m" << endl;
+    cout << "\n\033[0;36m    How many rounds?\033[0m" << endl;
+    int rounds = getInteger();
 
-    mainMenu(); // loop back to main menu
+    cout << "\033[0;36m    In a percentage, what's the chance of adding random number at start?\033[0m" << endl;
+    int headChance = getIntegerBetween(0, 100);
+
+    cout << "\033[0;36m    In a percentage, what's the chance of adding random number at end?\033[0m" << endl;
+    int tailChance = getIntegerBetween(0, 100);
+
+    // loop back to main menu
+    mainMenu(); 
 }
 
 void palindrome()
 {
-     
-    mainMenu(); // loop back to main menu
+    // menu section
+    string input; 
+    cout << "\n\033[36mWhat string would you like to make a palindrome?\033[0m" << endl;
+
+    cin.ignore(); // flush new line character
+    getline(cin, input);
+
+    // loop back to main menu
+    mainMenu(); 
 }
 
 
